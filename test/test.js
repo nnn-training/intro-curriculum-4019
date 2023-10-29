9431c54d9738456aaa05dde7238b966c2b72ae54
y.ito <yukitakaito1230@gmail.com>
2023-10-29 16:34:06 +0900

    練習問題対応

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
-'use strict';
-const request = require('supertest');
-const app = require('../app');
-const passportStub = require('passport-stub');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient({ log: [ 'query' ] });
+"use strict";
+const request = require("supertest");
+const app = require("../app");
+const passportStub = require("passport-stub");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient({ log: ["query"] });
 
-describe('/login', () => {
+describe("/login", () => {
   beforeAll(() => {
     passportStub.install(app);
-    passportStub.login({ username: 'testuser' });
+    passportStub.login({ username: "testuser" });
   });
 
   afterAll(() => {
@@ -16,36 +16,33 @@ describe('/login', () => {
     passportStub.uninstall();
   });
 
-  test('ログインのためのリンクが含まれる', async () => {
+  test("ログインのためのリンクが含まれる", async () => {
     await request(app)
-      .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
+      .get("/login")
+      .expect("Content-Type", "text/html; charset=utf-8")
       .expect(/<a href="\/auth\/github"/)
       .expect(200);
   });
 
-  test('ログイン時はユーザ名が表示される', async () => {
+  test("ログイン時はユーザ名が表示される", async () => {
     await request(app)
-      .get('/login')
+      .get("/login")
       .expect(/testuser/)
       .expect(200);
   });
 });
 
-describe('/logout', () => {
-  test('/ にリダイレクトされる', async () => {
-    await request(app)
-      .get('/logout')
-      .expect('Location', '/')
-      .expect(302);
+describe("/logout", () => {
+  test("/ にリダイレクトされる", async () => {
+    await request(app).get("/logout").expect("Location", "/").expect(302);
   });
 });
 
-describe('/schedules', () => {
-  let scheduleId = '';
+describe("/schedules", () => {
+  let scheduleId = "";
   beforeAll(() => {
     passportStub.install(app);
-    passportStub.login({ id: 0, username: 'testuser' });
+    passportStub.login({ id: 0, username: "testuser" });
   });
 
   afterAll(async () => {
@@ -54,26 +51,27 @@ describe('/schedules', () => {
     await deleteScheduleAggregate(scheduleId);
   });
 
-  test('予定が作成でき、表示される', async () => {
-    const userId = 0, username = 'testuser';
+  test("予定が作成でき、表示される", async () => {
+    const userId = 0,
+      username = "testuser";
     const data = { userId, username };
     await prisma.user.upsert({
       where: { userId },
       create: data,
-      update: data
+      update: data,
     });
     const res = await request(app)
-      .post('/schedules')
+      .post("/schedules")
       .send({
-        scheduleName: 'テスト予定1',
-        memo: 'テストメモ1\r\nテストメモ2',
-        candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3'
+        scheduleName: "テスト予定1",
+        memo: "テストメモ1\r\nテストメモ2",
+        candidates: "テスト候補1\r\nテスト候補2\r\nテスト候補3",
       })
-      .expect('Location', /schedules/)
+      .expect("Location", /schedules/)
       .expect(302);
 
     const createdSchedulePath = res.headers.location;
-    scheduleId = createdSchedulePath.split('/schedules/')[1];
+    scheduleId = createdSchedulePath.split("/schedules/")[1];
     await request(app)
       .get(createdSchedulePath)
       .expect(/テスト予定1/)
@@ -86,11 +84,11 @@ describe('/schedules', () => {
   });
 });
 
-describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
-  let scheduleId = '';
+describe("/schedules/:scheduleId/users/:userId/candidates/:candidateId", () => {
+  let scheduleId = "";
   beforeAll(() => {
     passportStub.install(app);
-    passportStub.login({ id: 0, username: 'testuser' });
+    passportStub.login({ id: 0, username: "testuser" });
   });
 
   afterAll(async () => {
@@ -99,25 +97,39 @@ describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
     await deleteScheduleAggregate(scheduleId);
   });
 
-  test('出欠が更新できる', async () => {
-    const userId = 0, username = 'testuser';
+  test("出欠が更新できる", async () => {
+    const userId = 0,
+      username = "testuser";
     const data = { userId, username };
     await prisma.user.upsert({
       where: { userId },
       create: data,
-      update: data
+      update: data,
     });
     const res = await request(app)
-      .post('/schedules')
-      .send({ scheduleName: 'テスト出欠更新予定1', memo: 'テスト出欠更新メモ1', candidates: 'テスト出欠更新候補1' });
+      .post("/schedules")
+      .send({
+        scheduleName: "テスト出欠更新予定1",
+        memo: "テスト出欠更新メモ1",
+        candidates: "テスト出欠更新候補1",
+      });
     const createdSchedulePath = res.headers.location;
-    scheduleId = createdSchedulePath.split('/schedules/')[1];
-    const candidate = await prisma.candidate.findFirst({ where: { scheduleId } });
+    scheduleId = createdSchedulePath.split("/schedules/")[1];
+    const candidate = await prisma.candidate.findFirst({
+      where: { scheduleId },
+    });
     // 更新がされることをテスト
     await request(app)
-      .post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`)
+      .post(
+        `/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`
+      )
       .send({ availability: 2 }) // 出席に更新
       .expect('{"status":"OK","availability":2}');
+    const availabilities = await prisma.availability.findMany({
+      where: { scheduleId },
+    });
+    expect(availabilities.length).toBe(1);
+    expect(availabilities[0].availability).toBe(2);
   });
 });
 
